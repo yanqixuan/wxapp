e386ca9c5c8628cb462e84bc89cd285499bfe0a7
yanqixuan <48020563+yanqixuan@users.noreply.github.com>
2019-05-04 21:10:43 +0800

    Add files via upload

--- msgbox_组件/pages/index/index.js
+++ msgbox_组件/pages/index/index.js
@@ -11,5 +11,15 @@
     },
     _success(){
       this.popup.hidePopup();
+    },
+    change:function(e){
+      // console.log('catch')
+      var mComponent = this.selectComponent('#mComponent');
+      mComponent.setText('外部调用了')
+    },
+    onTextChange:function(){
+      wx.showToast({
+        title:'捕获事件'
+      })
     }
   })
